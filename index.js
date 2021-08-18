d2e758da234926f4f172601fa98c863237880e1a
TeachY0u <marianoteach@rocketmail.com>
2021-08-17 23:55:18 -0700

    working on index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // import employee classes
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { error } = require('console');
+const generatedHtmlFilePath = './dist/TeamProfile.html'
+
+let teamMembers = [];
 
 inquirer
 .prompt([
@@ -25,12 +30,67 @@ inquirer
         },
         {
         type: 'list',
-        name: 'additionalTeamMember',
+        name: 'additionalTeamMembers',
         message: 'Select team members to add',
         choices: ["Engineer", "Intern"]
         },
 ])
 
 .then(answers => {
+    let manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber);
 
-})
+    teamMembers.push(manager);
+
+.catch(error => {
+    if(error.isTtyError) {
+    } else {
+    }
+});
+
+function addIntern(){
+    inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'internName',
+        message: "Enter inters name",
+        },
+        {
+        type: 'input',
+        name: 'internID',
+        message: "Enter interns employee ID.",
+        },
+        {
+        type: 'input',
+        name: 'internEmail',
+        message: 'Enter interns email',
+        },
+        {
+        type: 'input',
+        name: 'internSchool',
+        message: 'Enter interns school',
+        },
+        {
+        type: 'list',
+        name: 'additionalTeamMembers',
+        message: 'Select team members to add',
+        choices: ["Engineer", "Intern", "Exit"]
+        },
+])
+
+.then(answers => {
+    let intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
+
+    teamMembers.push(intern);
+
+
+.catch(error => {
+    if(error.isTtyError) {
+    } else {
+    }
+});
+
+}
+
+
+function generateHTML()
